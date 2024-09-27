38a448d0e74285133fa410cd2d652f500ba892de
Janhavi <janhaviganorkar12@gmail.com>
2024-09-27 21:51:03 +0530

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,244 @@
+export const Bio = {
+    name: "Janhavi Ganorkar",
+    roles: [
+      "Full Stack Developer",
+      "Frontend Web Developer",
+      "React Js Developer",
+      "Python Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Riyaman",
+    resume:
+      "https://drive.google.com/file/d/14UgTJBHY36hBnDb2nsEPyKM_27cEq2CI/view",
+    linkedin: "https://www.linkedin.com/in/janhavi-ganorkar-263752294/",
+    twitter: "https://x.com/janhaviganorkar",
+    insta: "https://www.instagram.com/itz_janhavi_official/",
+    facebook: "https://www.facebook.com/janhavi.ganorkar?mibextid=ZbWKwL",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        {
+          name: "Next Js",
+          image:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+       
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Django",
+          image: "DjangoLogo.png",
+        },
+      ],
+    },
+   
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://imgs.search.brave.com/hHpm-bAKw7C2sZzlUCJrr6GUrl-fnWUWZjKMaJXV_o4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODQ3Zjk4MWNlZjEw/MTRjMGI1ZTQ4YmUu/cG5n",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+
+        {
+          name: "Figma",
+          image:
+            "https://imgs.search.brave.com/vivqBJZIe-PAXZVARYFCXGN2nDPAfKLH7Jy5DGfyyck/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZnJlZWJpZXN1cHBs/eS5jb20vbG9nb3Mv/bGFyZ2UvMngvZmln/bWEtMS1sb2dvLXBu/Zy10cmFuc3BhcmVu/dC5wbmc",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://www.kodnest.com/wp-content/uploads/2022/12/3.png",
+      role: "Student Intern Full Stack Development",
+      company: "KodNest",
+      date: "July 2023 - Present",
+      desc: "Full Stack Development Training",
+      skills: [
+        "Java Programming",
+        "Manual Testing",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React Js",
+        "SQL",
+        "Spring",
+        "Communication",
+        "Github",
+      ],
+    },
+    {
+      id: 1,
+      img: "https://letsgrowmore.in/wp-content/uploads/2021/05/Artboard-1-1-removebg-preview-e1645900071758.jpg",
+      role: "Web Development Intern",
+      company: "LetsGrowMore",
+      date: "March 2023 - April 2023",
+      desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],  
+    },
+    {
+      id: 2,
+      img: "png-clipart-logo-community-text-logo.png",
+      role: "Frontend Developer Intern",
+      company: "ASReaPr pvt ltd",
+      date: "November 2022 - March 2023",
+      desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Docker",
+        "SDLC",
+        "MongoDB",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "vmvlogo.jpg",
+      school: "Govenment vidarbh institute of science and humanity",
+      date: "August 2021 - April 2024",
+      grade: "8.60 CGPA",
+      desc: "I completed my Bachelor's degree in Computer Science at Govenment vidarbh institute of science and humanity.",
+      degree: "Bachelor of Technology - Bsc",
+    },
+    {
+      id: 1,
+      img: "https://sch.edu.in/wp-content/uploads/2021/11/SCHLOGO.png",
+      school: "Bhagyashree Vidhayalaya & Junior College Amravati",
+      date: "August 2017 - April 2019",
+      grade: "45%",
+      desc: "I completed my class 12 high school education at Bhagyashree Vidhayalaya & Junior College Amravati",
+      degree: "Intermediate, Science(PCMB)",
+    },
+    {
+      id: 2,
+      img: "https://www.nuaodisha.com/images/Contents/NuaOdisha-136370-Saraswati-VidyaMandir.png",
+      school: "Bhagyashree Vidhayalaya & Junior College Amravati",
+      date: "April 2016 - April 2017",
+      grade: "62.42%",
+      desc: "I completed my class 10 education at Bhagyashree Vidhayalaya & Junior College Amravati.",
+      degree: "Matriculation",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Animated Website",
+      date: "Jun 2024",
+      // description:
+      //   "An Community platform for electronics enthusiasts were they can find the each and every solution of their problems and also they can manage & publish their projects",
+      image:
+        "project1.png",
+      tags: [
+        "React Js",
+        "Redux",
+        "Boostrap",
+        "Talwind CSS",
+        "Node Js",
+        "Git",
+        "Docker", 
+      ],
+      category: "website",
+      github: "https://github.com/Riyaman/animated-website",
+      
+    },
+    {
+      id: 0,
+      title: "Online Food Delivery",
+      date: "June 2024",
+      // description:
+      //   "An online platform for testing and showcasing user-created HTML, CSS and JavaScript code snippets.",
+      image:
+        "Food.png",
+      tags: ["HTML", "CSS"],
+      category: "website",
+      github: "https://riyaman.github.io/restorent_web/",
+      
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2019, text: "Started my journey as Developer" },
+    { year: 2019, text: "Learning Web development" },
+    { year: 2021, text: "Learning React Js" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2022, text: "Started my journy as Frontend Developer Intern" },
+    { year: 2023, text: "Started my own platform" },
+  ];
